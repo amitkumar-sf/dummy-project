316e740ae9f67d18dce9b12b8392b79429e87349
Amit Kumar <amit.kumar5@sourcefuse.com>
2021-12-03 14:10:31 +0530

    added both libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { GanttModule } from '@syncfusion/ej2-angular-gantt';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([]),
+    GanttModule
   ],
   providers: [],
   bootstrap: [AppComponent]
